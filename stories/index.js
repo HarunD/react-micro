0b2f088a7d22a7dbcab55cf058c29f35700a7566
Harun Dulic <harundulic@gmail.com>
2018-03-20 10:57:58 +0100

    Removed unused Button import from stories

--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import {storiesOf} from '@storybook/react'
-import {Button} from '@storybook/react/demo'
+import React from 'react';
+import {storiesOf} from '@storybook/react';
 
 import Micro from './../src/Micro';
 
